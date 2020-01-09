c63a539da8af2e0b985e776b38d3455954021ce7
HIROKI KOBAYASHI <hiroki@front-works.com>
2020-01-09 15:50:15 +0900

    renewal

---
+++ assets/scripts/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+  // smooth scroll
+  var fixedTopHeight = 0;
+  $("a[href^='#']").not('[target^=none]').on("click", function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - fixedTopHeight;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+
+}(jQuery));
